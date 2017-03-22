02c1fe6e2dbfc6afa2d65adbb602dd0090b98e42
kirankn <kirankn8@gmail.com>
2017-03-22 11:00:28 +0530

    Adding RequestMapping to access googlemaps data in json format

--- TripPlanner/src/tripPlanner/controllers/HomeController.java
+++ TripPlanner/src/tripPlanner/controllers/HomeController.java
@@ -1,13 +1,38 @@
 package tripPlanner.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.google.maps.model.TravelMode;
+import com.kirankn.studentadmissioncontroller.Student;
+
+import tripPlanner.interfaces.MapService;
+import tripPlanner.interfaces.PlanRoundTripInterface;
+import tripPlanner.models.GoogleDirections;
+import tripPlanner.services.googlemaps.GoogleMapService;
+
+@RestController
 public class HomeController {
  
     @RequestMapping(value = "/")
     public String index() {
         return "index.html";
     }
+    
+    
+    /*
+     * Incomplete mapping.........................
+     */
+    @RequestMapping(value="/mapsdata",method = RequestMethod.GET, produces = "application/json")
+    public GoogleDirections mapsdata(String origin, ArrayList<String> waypoints) throws Exception {
+    	MapService m = new GoogleMapService();
+    	PlanRoundTripInterface p;
+    	p = m.getService(origin, waypoints);
+		return p.calcRoundTrip(origin, waypoints, TravelMode.DRIVING);
+    }
+    
 }
