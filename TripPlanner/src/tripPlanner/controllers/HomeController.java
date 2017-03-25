207dab69c791f826b6601e3d7f68c7ab92bdff6a
kirankn <kirankn8@gmail.com>
2017-03-25 13:51:00 +0530

    Adding maps view

--- TripPlanner/src/tripPlanner/controllers/HomeController.java
+++ TripPlanner/src/tripPlanner/controllers/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.maps.model.DirectionsResult;
 import com.google.maps.model.TravelMode;
 
 import tripPlanner.config.BeanConfig;
@@ -16,21 +17,42 @@ import tripPlanner.models.GoogleDirections;
 @RestController
 public class HomeController {
  
-    @RequestMapping(value = "/")
+    @RequestMapping(value = "/hello")
     public String index() {
         return "index.html";
     }
     
     
+    
+    
+    
     /*
      * Incomplete mapping.........................
-     */
+    
     @RequestMapping(value="/mapsdata",method = RequestMethod.GET, produces = "application/json")
     public GoogleDirections mapsdata(String origin, ArrayList<String> waypoints) throws Exception 
     {
     	AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(BeanConfig.class);
     	PlanRoundTripInterface pt = ctx.getBean(PlanRoundTripInterface.class);
     	return pt.calcRoundTrip(origin, waypoints, TravelMode.DRIVING);
+    } */
+    
+    
+    /*temporary mapping*/
+    @RequestMapping(value="/")
+    public DirectionsResult mapsdata() throws Exception 
+    {
+    	String origin = "Bangalore";
+    	ArrayList<String> waypoints = new ArrayList<String>();		
+    	waypoints.add("Mysore");
+    	waypoints.add("Andhra Pradesh");
+    	waypoints.add("Telangana");
+    	waypoints.add("Goa");
+    	AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(BeanConfig.class);
+    	PlanRoundTripInterface pt = ctx.getBean(PlanRoundTripInterface.class);
+    	ctx.close();
+    	return pt.calcRoundTrip(origin, waypoints, TravelMode.DRIVING).getResult();
+    	
     }
     
 }
